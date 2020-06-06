7d0902443c310cfe98da5d6ab900e2896c602b0f
Rahul RajYadav <33296488+rr-y@users.noreply.github.com>
2020-06-06 13:02:40 +0530

    builtIn polymorphism example

---
+++ polymorphism.py
@@ -0,0 +1,9 @@
+'''
+Same function with different form is called polymorphism.
+BuiltIn function len() can be used with string and array as well.
+
+'''
+
+a = "rahul raj yadav"
+b = [1, 5, 3, 9, 7, 2]
+print(len(a)," ",len(b))
